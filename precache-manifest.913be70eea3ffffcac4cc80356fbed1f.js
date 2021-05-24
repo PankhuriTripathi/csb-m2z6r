cd69fc58141911f5ab64ee785f2cf67dd088a98a
Code Sandbox <hello@codesandbox.io>
2021-05-24 16:00:16 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.913be70eea3ffffcac4cc80356fbed1f.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "606b6e71728b33fcdbf1ee1818ab3dec",
+    "url": "https://pankhuritripathi.github.io/csb-m2z6r/index.html"
+  },
+  {
+    "revision": "a48f9d490ac6c260b2fd",
+    "url": "https://pankhuritripathi.github.io/csb-m2z6r/static/js/2.41d2c938.chunk.js"
+  },
+  {
+    "revision": "128d7c121b6a474d5390",
+    "url": "https://pankhuritripathi.github.io/csb-m2z6r/static/js/main.474bea32.chunk.js"
+  },
+  {
+    "revision": "27e9b39accd937fa773e",
+    "url": "https://pankhuritripathi.github.io/csb-m2z6r/static/js/runtime-main.1d348817.js"
+  }
+]);
